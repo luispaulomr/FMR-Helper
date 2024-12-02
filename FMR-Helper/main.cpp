5809690f60a9074dde0bd1d91ecd984d406ddf34
Luis Ribeiro <luispaulomr@gmail.com>
2024-12-02 00:59:26 -0300

    Nightly.

--- FMR-Helper/main.cpp
+++ FMR-Helper/main.cpp
@@ -49,6 +49,12 @@ typedef struct WindowData {
     uint32_t CounterExecute = 0;
     uint32_t CounterExecuteMax = 20;
 
+    /* Log counter */
+
+    uint32_t CounterLog = 0;
+    uint32_t CounterLogMax = 200;
+    bool isAttached = false;
+
     /* Our game attach/read class */
 
     CModGame ModGame;
@@ -217,22 +223,230 @@ static void ShowFusionsWindow(WindowData_t& wd)
     const ImGuiViewport* viewport = ImGui::GetMainViewport();
     ImVec2 work_pos = viewport->WorkPos; // Use work area to avoid menu-bar/task-bar, if any!
     ImVec2 work_size = viewport->WorkSize;
-    ImVec2 window_pos;
-    ImVec2 window_size;
-    window_pos.x = work_pos.x;
-    window_pos.y = work_pos.y;
-    ImGui::SetNextWindowPos(window_pos, ImGuiCond_Always);
-    //window_size = ImVec2(max_cards * (image_width + 10.0) + image_width + 15.0, num_fusions * (image_height + 75.0));
-    window_size = ImVec2(500.0, 500.0);
-    ImGui::SetNextWindowSize(window_size, ImGuiCond_Always);
+    ImGui::SetNextWindowPos(ImVec2(work_pos.x, work_pos.y), ImGuiCond_Always);
+    ImGui::SetNextWindowSize(ImVec2(500.0, 500.0), ImGuiCond_Always);
 
     if (ImGui::Begin("Fusions", &wd.ShowFusions, window_flags))
     {
-        for (const auto& fusion : wd.Fusions) {
-            ShowFusion(wd, fusion);
+        if (wd.ImageTextures.size()) {
+            for (const auto& fusion : wd.Fusions) {
+                ShowFusion(wd, fusion);
+            }
         }
     }
     ImGui::End();
+}
+
+struct Console
+{
+    char                  InputBuf[256];
+    ImVector<char*>       Items;
+    ImVector<const char*> Commands;
+    ImVector<char*>       History;
+    int                   HistoryPos;    // -1: new line, 0..History.Size-1 browsing history.
+    ImGuiTextFilter       Filter;
+    bool                  AutoScroll;
+    bool                  ScrollToBottom;
+
+    Console()
+    {
+        ClearLog();
+        memset(InputBuf, 0, sizeof(InputBuf));
+        HistoryPos = -1;
+
+        // "CLASSIFY" is here to provide the test case where "C"+[tab] completes to "CL" and display multiple matches.
+        //Commands.push_back("HELP");
+        //Commands.push_back("HISTORY");
+        //Commands.push_back("CLEAR");
+        //Commands.push_back("CLASSIFY");
+        AutoScroll = true;
+        ScrollToBottom = false;
+        //AddLog("Welcome to Dear ImGui!");
+    }
+    ~Console()
+    {
+        ClearLog();
+        for (int i = 0; i < History.Size; i++) {
+            ImGui::MemFree(History[i]);
+        }
+    }
+
+    // Portable helpers
+    static int   Stricmp(const char* s1, const char* s2) { int d; while ((d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; } return d; }
+    static int   Strnicmp(const char* s1, const char* s2, int n) { int d = 0; while (n > 0 && (d = toupper(*s2) - toupper(*s1)) == 0 && *s1) { s1++; s2++; n--; } return d; }
+    static char* Strdup(const char* s) { IM_ASSERT(s); size_t len = strlen(s) + 1; void* buf = ImGui::MemAlloc(len); IM_ASSERT(buf); return (char*)memcpy(buf, (const void*)s, len); }
+    static void  Strtrim(char* s) { char* str_end = s + strlen(s); while (str_end > s && str_end[-1] == ' ') str_end--; *str_end = 0; }
+
+    void ClearLog()
+    {
+        for (int i = 0; i < Items.Size; i++) {
+            ImGui::MemFree(Items[i]);
+        }
+        Items.clear();
+    }
+
+    void AddLog(const char* fmt, ...) IM_FMTARGS(2)
+    {
+        // FIXME-OPT
+        char buf[1024];
+        va_list args;
+        va_start(args, fmt);
+        vsnprintf(buf, IM_ARRAYSIZE(buf), fmt, args);
+        buf[IM_ARRAYSIZE(buf) - 1] = 0;
+        va_end(args);
+        Items.push_back(Strdup(buf));
+    }
+
+    void Draw(const char* title, bool* p_open)
+    {
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoSavedSettings;
+        const ImGuiViewport* viewport = ImGui::GetMainViewport();
+        ImVec2 work_pos = viewport->WorkPos;
+        ImVec2 work_size = viewport->WorkSize;
+        ImGui::SetNextWindowPos(ImVec2(work_pos.x + 500.0, work_pos.y), ImGuiCond_Always);
+        ImGui::SetNextWindowSize(ImVec2(500.0, 500.0), ImGuiCond_Always);
+        if (!ImGui::Begin(title, p_open, window_flags))
+        {
+            ImGui::End();
+            return;
+        }
+
+        // As a specific feature guaranteed by the library, after calling Begin() the last Item represent the title bar.
+        // So e.g. IsItemHovered() will return true when hovering the title bar.
+        // Here we create a context menu only available from the title bar.
+        //if (ImGui::BeginPopupContextItem())
+        //{
+        //    if (ImGui::MenuItem("Close Console"))
+        //        *p_open = false;
+        //    ImGui::EndPopup();
+        //}
+
+        //ImGui::TextWrapped(
+        //    "This example implements a console with basic coloring, completion (TAB key) and history (Up/Down keys). A more elaborate "
+        //    "implementation may want to store entries along with extra data such as timestamp, emitter, etc.");
+        //ImGui::TextWrapped("Enter 'HELP' for help.");
+
+        // TODO: display items starting from the bottom
+
+        //if (ImGui::SmallButton("Add Debug Text")) { AddLog("%d some text", Items.Size); AddLog("some more text"); AddLog("display very important message here!"); }
+        //ImGui::SameLine();
+        //if (ImGui::SmallButton("Add Debug Error")) { AddLog("[error] something went wrong"); }
+        //ImGui::SameLine();
+        //if (ImGui::SmallButton("Clear")) { ClearLog(); }
+        //ImGui::SameLine();
+        //bool copy_to_clipboard = ImGui::SmallButton("Copy");
+        //static float t = 0.0f; if (ImGui::GetTime() - t > 0.02f) { t = ImGui::GetTime(); AddLog("Spam %f", t); }
+
+        //ImGui::Separator();
+
+        //// Options menu
+        //if (ImGui::BeginPopup("Options"))
+        //{
+        //    ImGui::Checkbox("Auto-scroll", &AutoScroll);
+        //    ImGui::EndPopup();
+        //}
+
+        //// Options, Filter
+        //ImGui::SetNextItemShortcut(ImGuiMod_Ctrl | ImGuiKey_O, ImGuiInputFlags_Tooltip);
+        //if (ImGui::Button("Options"))
+        //    ImGui::OpenPopup("Options");
+        //ImGui::SameLine();
+        //Filter.Draw("Filter (\"incl,-excl\") (\"error\")", 180);
+        //ImGui::Separator();
+
+        // Reserve enough left-over height for 1 separator + 1 input text
+        const float footer_height_to_reserve = ImGui::GetStyle().ItemSpacing.y + ImGui::GetFrameHeightWithSpacing();
+        if (ImGui::BeginChild("ScrollingRegion", ImVec2(0, -footer_height_to_reserve), ImGuiChildFlags_NavFlattened, ImGuiWindowFlags_HorizontalScrollbar))
+        {
+            if (ImGui::BeginPopupContextWindow())
+            {
+                if (ImGui::Selectable("Clear")) ClearLog();
+                ImGui::EndPopup();
+            }
+
+            // Display every line as a separate entry so we can change their color or add custom widgets.
+            // If you only want raw text you can use ImGui::TextUnformatted(log.begin(), log.end());
+            // NB- if you have thousands of entries this approach may be too inefficient and may require user-side clipping
+            // to only process visible items. The clipper will automatically measure the height of your first item and then
+            // "seek" to display only items in the visible area.
+            // To use the clipper we can replace your standard loop:
+            //      for (int i = 0; i < Items.Size; i++)
+            //   With:
+            //      ImGuiListClipper clipper;
+            //      clipper.Begin(Items.Size);
+            //      while (clipper.Step())
+            //         for (int i = clipper.DisplayStart; i < clipper.DisplayEnd; i++)
+            // - That your items are evenly spaced (same height)
+            // - That you have cheap random access to your elements (you can access them given their index,
+            //   without processing all the ones before)
+            // You cannot this code as-is if a filter is active because it breaks the 'cheap random-access' property.
+            // We would need random-access on the post-filtered list.
+            // A typical application wanting coarse clipping and filtering may want to pre-compute an array of indices
+            // or offsets of items that passed the filtering test, recomputing this array when user changes the filter,
+            // and appending newly elements as they are inserted. This is left as a task to the user until we can manage
+            // to improve this example code!
+            // If your items are of variable height:
+            // - Split them into same height items would be simpler and facilitate random-seeking into your list.
+            // - Consider using manual call to IsRectVisible() and skipping extraneous decoration from your items.
+            ImGui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2(4, 1)); // Tighten spacing
+            //if (copy_to_clipboard)
+            //    ImGui::LogToClipboard();
+            for (const char* item : Items)
+            {
+                if (!Filter.PassFilter(item))
+                    continue;
+
+                // Normally you would store more information in your item than just a string.
+                // (e.g. make Items[] an array of structure, store color/type etc.)
+                ImVec4 color;
+                bool has_color = false;
+                if (strstr(item, "[error]")) { color = ImVec4(1.0f, 0.4f, 0.4f, 1.0f); has_color = true; }
+                else if (strncmp(item, "# ", 2) == 0) { color = ImVec4(1.0f, 0.8f, 0.6f, 1.0f); has_color = true; }
+                if (has_color)
+                    ImGui::PushStyleColor(ImGuiCol_Text, color);
+                ImGui::TextUnformatted(item);
+                if (has_color)
+                    ImGui::PopStyleColor();
+            }
+            //if (copy_to_clipboard)
+            //    ImGui::LogFinish();
+
+            // Keep up at the bottom of the scroll region if we were already at the bottom at the beginning of the frame.
+            // Using a scrollbar or mouse-wheel will take away from the bottom edge.
+            if (ScrollToBottom || (AutoScroll && ImGui::GetScrollY() >= ImGui::GetScrollMaxY()))
+                ImGui::SetScrollHereY(1.0f);
+            ScrollToBottom = false;
+
+            ImGui::PopStyleVar();
+        }
+        ImGui::EndChild();
+        //ImGui::Separator();
+
+        //// Command-line
+        //bool reclaim_focus = false;
+        //ImGuiInputTextFlags input_text_flags = ImGuiInputTextFlags_EnterReturnsTrue | ImGuiInputTextFlags_EscapeClearsAll | ImGuiInputTextFlags_CallbackCompletion | ImGuiInputTextFlags_CallbackHistory;
+        //if (ImGui::InputText("Input", InputBuf, IM_ARRAYSIZE(InputBuf), input_text_flags, &TextEditCallbackStub, (void*)this))
+        //{
+        //    char* s = InputBuf;
+        //    Strtrim(s);
+        //    if (s[0])
+        //        ExecCommand(s);
+        //    strcpy(s, "");
+        //    reclaim_focus = true;
+        //}
+
+        // Auto-focus on window apparition
+        //ImGui::SetItemDefaultFocus();
+        //if (reclaim_focus)
+        //    ImGui::SetKeyboardFocusHere(-1); // Auto focus previous widget
+
+        ImGui::End();
+    }
+};
+
+static void ShowConsole(Console& console, bool* p_open)
+{
+    console.Draw("Console", p_open);
 }
 
 // Main code
@@ -295,6 +509,8 @@ int main(int, char**)
     WindowData_t wd;
     wd.ModGame.Attach(L"ePSXe - Enhanced PSX Emulator", L"ePSXe.exe");
 
+    Console console;
+
     // Main loop
     bool done = false;
     while (!done)
@@ -343,9 +559,24 @@ int main(int, char**)
             wd.CounterExecute = 0;
         }
 
-        if (wd.ImageTextures.size()) {
-            ShowFusionsWindow(wd);
+        ++wd.CounterLog;
+
+        if (wd.CounterLog == wd.CounterLogMax) {
+            if (!wd.ModGame.IsAttached()) {
+                console.AddLog("Trying to attach to ePSXe...");
+                wd.isAttached = false;
+            }
+            else {
+                if (!wd.isAttached) {
+                    console.AddLog("Attached to ePSXe.");
+                }
+                wd.isAttached = true;
+            }
+            wd.CounterLog = 0;
         }
+
+        ShowFusionsWindow(wd);
+        ShowConsole(console, &wd.ShowConsole);
 
         // Rendering
         ImGui::Render();
